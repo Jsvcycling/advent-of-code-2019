a7c6469107d71aa175b5df0c9e653e0ee5be44bc
Josh Vega <jsvcycling.live@gmail.com>
2019-12-13 18:47:08 -0500

    Day 3: Complete!

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -2,6 +2,12 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Debug)]
+struct Step<'a> {
+    pub direction: &'a str,
+    pub distance: i32,
+}
+
 fn part1(lines: &Vec<String>) -> i32 {
     let mut data: HashMap<(i32, i32), [bool; 2]> = HashMap::new();
 
@@ -9,18 +15,24 @@ fn part1(lines: &Vec<String>) -> i32 {
         let mut endpoints: Vec<(i32, i32)> = Vec::new();
         endpoints.push((0, 0));
 
-        let steps: Vec<String> = line.split(",").map(|v| String::from(v.trim())).collect();
+        let steps: Vec<Step> = line
+            .split(",")
+            .map(|v| {
+                let (dir, amt) = v.trim().split_at(1);
+                Step {
+                    direction: dir,
+                    distance: amt.parse().unwrap(),
+                }
+            })
+            .collect();
 
         for step in steps {
-            let (dir, amt_str) = step.split_at(1);
-            let amt: i32 = amt_str.parse().unwrap();
-
             let last = endpoints.last().unwrap().clone();
 
-            match dir {
+            match step.direction {
                 "U" => {
                     let start = last.1 + 1;
-                    let end = last.1 + amt;
+                    let end = last.1 + step.distance;
 
                     for idx in start..=end {
                         let val = data.entry((last.0, idx)).or_insert([false; 2]);
@@ -28,9 +40,9 @@ fn part1(lines: &Vec<String>) -> i32 {
                     }
 
                     endpoints.push((last.0, end));
-                },
+                }
                 "D" => {
-                    let start = last.1 - amt;
+                    let start = last.1 - step.distance;
                     let end = last.1 - 1;
 
                     for idx in start..=end {
@@ -39,9 +51,9 @@ fn part1(lines: &Vec<String>) -> i32 {
                     }
 
                     endpoints.push((last.0, start));
-                },
+                }
                 "L" => {
-                    let start = last.0 - amt;
+                    let start = last.0 - step.distance;
                     let end = last.0 - 1;
 
                     for idx in start..=end {
@@ -50,10 +62,10 @@ fn part1(lines: &Vec<String>) -> i32 {
                     }
 
                     endpoints.push((start, last.1));
-                },
+                }
                 "R" => {
                     let start = last.0 + 1;
-                    let end = last.0 + amt;
+                    let end = last.0 + step.distance;
 
                     for idx in start..=end {
                         let val = data.entry((idx, last.1)).or_insert([false; 2]);
@@ -61,26 +73,142 @@ fn part1(lines: &Vec<String>) -> i32 {
                     }
 
                     endpoints.push((end, last.1));
-                },
+                }
                 _ => panic!(),
             };
         }
     }
 
-    let mut dists = data.iter()
+    let mut dists = data
+        .iter()
         .filter(|(_, v)| v[0] && v[1])
         .map(|(k, _)| k.0.abs() + k.1.abs())
         .collect::<Vec<i32>>();
 
     dists.sort();
 
-    println!("{:?}", dists);
-
     dists.first().unwrap().clone()
 }
 
 fn part2(lines: &Vec<String>) -> i32 {
-    0
+    // We want to store both whether each line has visited a point as well as
+    // how long it took to get there the first time.
+    let mut data: HashMap<(i32, i32), [(bool, i32); 2]> = HashMap::new();
+
+    for (line_idx, line) in lines.iter().enumerate() {
+        let mut i = 1;
+        let mut endpoints: Vec<(i32, i32)> = Vec::new();
+        endpoints.push((0, 0));
+
+        let steps: Vec<Step> = line
+            .split(",")
+            .map(|v| {
+                let (dir, amt) = v.trim().split_at(1);
+                Step {
+                    direction: dir,
+                    distance: amt.parse().unwrap(),
+                }
+            })
+            .collect();
+
+        for step in steps {
+            let last = endpoints.last().unwrap().clone();
+
+            match step.direction {
+                "U" => {
+                    let start = last.1 + 1;
+                    let end = last.1 + step.distance;
+
+                    for idx in start..=end {
+                        let val = data
+                            .entry((last.0, idx))
+                            .or_insert([(false, std::i32::MAX); 2]);
+
+                        val[line_idx].0 = true;
+
+                        if val[line_idx].1 == std::i32::MAX {
+                            val[line_idx].1 = i;
+                        }
+
+                        i += 1;
+                    }
+
+                    endpoints.push((last.0, end));
+                }
+                "D" => {
+                    let start = last.1 - step.distance;
+                    let end = last.1 - 1;
+
+                    for idx in start..=end {
+                        let val = data
+                            .entry((last.0, idx))
+                            .or_insert([(false, std::i32::MAX); 2]);
+
+                        val[line_idx].0 = true;
+
+                        if val[line_idx].1 == std::i32::MAX {
+                            val[line_idx].1 = i;
+                        }
+
+                        i += 1;
+                    }
+
+                    endpoints.push((last.0, start));
+                }
+                "L" => {
+                    let start = last.0 - step.distance;
+                    let end = last.0 - 1;
+
+                    for idx in start..=end {
+                        let val = data
+                            .entry((idx, last.1))
+                            .or_insert([(false, std::i32::MAX); 2]);
+
+                        val[line_idx].0 = true;
+
+                        if val[line_idx].1 == std::i32::MAX {
+                            val[line_idx].1 = i;
+                        }
+
+                        i += 1;
+                    }
+
+                    endpoints.push((start, last.1));
+                }
+                "R" => {
+                    let start = last.0 + 1;
+                    let end = last.0 + step.distance;
+
+                    for idx in start..=end {
+                        let val = data
+                            .entry((idx, last.1))
+                            .or_insert([(false, std::i32::MAX); 2]);
+
+                        val[line_idx].0 = true;
+
+                        if val[line_idx].1 == std::i32::MAX {
+                            val[line_idx].1 = i;
+                        }
+
+                        i += 1;
+                    }
+
+                    endpoints.push((end, last.1));
+                }
+                _ => panic!(),
+            };
+        }
+    }
+
+    let mut dists = data
+        .iter()
+        .filter(|(_, v)| v[0].0 && v[1].0)
+        .map(|(_, v)| v[0].1 + v[1].1)
+        .collect::<Vec<i32>>();
+
+    dists.sort();
+
+    dists.first().unwrap().clone()
 }
 
 fn main() {
